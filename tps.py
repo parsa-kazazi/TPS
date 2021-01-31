fc633e50eca8283d90ff444f6a0685c9842c8cdd
پارسا کزازی <77957421+parsa-kazazi@users.noreply.github.com>
2021-01-31 14:37:55 +0330

    Update tps.py

--- tps.py
+++ tps.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 # -*- coding: utf-8 -*-
-
 """
 Coded by parsa kazazi
 @parsa_kazazi (Github, Twitter)
@@ -12,18 +11,18 @@ Version: 1.0
 """
 
 import sys
-import time
-import os
 import socket
+import os
+import time
 
 os_name = os.name
 
 if (os_name == "nt"):
     os.system("cls")
-    os.system("title Port Scanner")
+    os.system("title TPS Port Scanner")
 else:
     os.system("clear")
-    os.system("printf '\033]2;Port Scanner\a'")
+    os.system("printf '\033]2;TPS Port Scanner\a'")
 
 print("""
 
@@ -33,7 +32,7 @@ print("""
        ██    ██           ██
        ██    ██      ███████
 
-    Quick and easy TCP Port Scanner (tps)
+    TCP Port Scanner. Quick and easy
 
 """)
 
@@ -43,7 +42,7 @@ good = str("\033[92m[+]\033[0m ")
 error = str("\033[91m[-]\033[0m ")
 
 def is_port_open(ip: str, port: int):
-    # check the port connection
+    # scan the one port
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # to create connection
     try:
         sock.connect((ip, port))
@@ -53,15 +52,15 @@ def is_port_open(ip: str, port: int):
     else:
         # if port was open
         return True
-    sock.close()
+    sock.close() # close the connection
 
 if (__name__ == "__main__"):
-    address = input(question + "Enter Target Domain or IP : ")
+    address = input(question + "Target Domain or IP address : ")
     time.sleep(2)
     print(info + "Checking connection to " + address + " ...")
     time.sleep(2)
     try:
-        ip = socket.gethostbyname(address)
+        ip = socket.gethostbyname(address) # get target IP address
     except socket.gaierror:
         print(error + address + " : Name or service not known\n")
         sys.exit()
@@ -72,15 +71,20 @@ if (__name__ == "__main__"):
         print(good + "Connected")
         time.sleep(1)
         print(info + "Host to scan : " + ip)
-        print(info + "Scanning. please wait...")
+        print(info + "Scanning. please wait...\n")
         time.sleep(2)
         port = 0
         open_ports = 0
         closed_ports = 0
         try:
-            for port in range(0, 65535):
+            for port in range(0, 65536):
                 if (is_port_open(ip, port)):
-                    print(good + "Port " + str(port) + " is open")
+                    try:
+                        service_name = socket.getservbyport(port)
+                    except OSError:
+                        # if port service name is not known
+                        service_name = "unknown service"
+                    print(good + "Port " + str(port) + " " + service_name + " : is open")
                     open_ports += 1
                 else:
                     closed_ports += 1
@@ -92,11 +96,11 @@ if (__name__ == "__main__"):
             sys.exit()
         else:
             time.sleep(2)
-            print(info + "Scan completed. result:")
+            print("\n" + info + "Scan completed. result:")
             time.sleep(1)
-            print(info + str(open_ports) + " Ports are open")
             if (open_ports == 0):
-                print(info + "All 65535 Ports are closed\n")
+                print(info + "All 65535 scanned ports are closed\n")
             else:
+                print(info + str(open_ports) + " Ports are open")
                 print(info + str(closed_ports) + " Ports are closed\n")
             time.sleep(1)
